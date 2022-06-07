AOSP-Krypton/packages_apps_Settings

Settings: applock: allow opting out of biometrics [2/2]
Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>

src/com/android/settings/security/applock/AppLockBiometricPreferenceController.kt
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricManager.Authenticators
+
+import androidx.preference.Preference
+import androidx.preference.SwitchPreference
+
+import com.krypton.settings.KryptonBasePreferenceController
+
+class AppLockBiometricPreferenceController(
+    context: Context,
+    key: String,
+) : KryptonBasePreferenceController(context, key),
+        Preference.OnPreferenceChangeListener {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private val biometricManager = context.getSystemService(BiometricManager::class.java)
+
+    override fun getAvailabilityStatus(): Int {
+        val biometricsAllowed = biometricManager.canAuthenticate(
+            Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
+        return if (biometricsAllowed)
+            AVAILABLE
+        else
+            UNSUPPORTED_ON_DEVICE
+    }
+
+    override fun updateState(preference: Preference) {
+        (preference as SwitchPreference).setChecked(appLockManager.isBiometricsAllowed())
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        appLockManager.setBiometricsAllowed(newValue as Boolean)
+        return true
+    }
+}

PixysOS-Beta/packages_apps_Settings-old

Signed-off-by: aswin7469 <aswinas@pixysos.com>
Change-Id: I91e72ceda7c3e5ea5f60be43760ca27f34e00cbb

src/com/android/settings/security/applock/AppLockBiometricPreferenceController.kt
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricManager.Authenticators
+
+import androidx.preference.Preference
+import androidx.preference.SwitchPreference
+
+import com.android.settings.pixys.PixysBasePreferenceController
+
+class AppLockBiometricPreferenceController(
+    context: Context,
+    key: String,
+) : PixysBasePreferenceController(context, key),
+        Preference.OnPreferenceChangeListener {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private val biometricManager = context.getSystemService(BiometricManager::class.java)
+
+    override fun getAvailabilityStatus(): Int {
+        val biometricsAllowed = biometricManager.canAuthenticate(
+            Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
+        return if (biometricsAllowed)
+            AVAILABLE
+        else
+            UNSUPPORTED_ON_DEVICE
+    }
+
+    override fun updateState(preference: Preference) {
+        (preference as SwitchPreference).setChecked(appLockManager.isBiometricsAllowed())
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        appLockManager.setBiometricsAllowed(newValue as Boolean)
+        return true
+    }
+}